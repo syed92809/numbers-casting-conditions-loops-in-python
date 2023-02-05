5ded67d55ff975041bb4287d6ddf60622f8ceee2
syed faizan <98455313+syed92809@users.noreply.github.com>
2023-02-05 06:37:06 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+#Numbers in python
+print("--------Numbers--------")
+var1=1
+var2="Faizan"
+var3=25.8
+var4=True
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+print("--------Casting--------")
+
+#Casting in python
+var1=21
+var1=float(21)
+print(var1)
+
+var2=20.5
+var2=int(20.5)
+print(var2)
+
+print("--------if else--------")
+
+#If Else in python
+a=20
+b=30
+
+if a>b:
+    print("A is greater than B")
+
+elif b>a:
+    print("B is greater than A")
+
+else:
+    print("A and B are equal")
+
+print("-------for loop---------")
+
+
+#Loops in python
+students=["Faizan","Ahsan","Taha","Asad"]
+for std in students:
+    print(std)
+
+print("-------while loop---------")
+i=0
+while i<=10:
+    print(i)
+    i=i+1
+
+print("-------------------")
